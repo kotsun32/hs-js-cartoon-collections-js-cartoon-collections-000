f230b6ec7842fec67c1eeed3bf2147b8d8c5830d
Sunny Kotwal <sunnykotwal@yahoo.com>
2018-02-09 21:36:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function dwarfRollCall(dwarves) {
+var array = []
+for (let i = 0; i < dwarves.length; i++) {
+array.push( `${i+1}. ${dwarves[i]} `)
+}
+return array.join(``)
 }
 
 function summonCaptainPlanet(planeteerCalls){
